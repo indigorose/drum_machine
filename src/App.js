a71a10d3016e312018850b78b3c318191fa824f1
indigorose <indigorose049@gmail.com>
2022-10-14 21:35:32 +0100

    Initial commit and planning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" id="drum-machine">
+        <div className="display-pad">
+          {/* all drum pads are clickable events */}
+          <div className="drum-pad" id="pad-Q heater-1">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+              className="clip"
+              id="Q"
+            ></audio>
+            "Q"
+          </div>
+          <div className="drum-pad" id="pad-W heater-2">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+              className="clip"
+              id="W"
+            ></audio>
+            "W"
+          </div>
+          <div className="drum-pad" id="pad-E heater-3">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+              className="clip"
+              id="E"
+            ></audio>
+            "E"
+          </div>
+          <div className="drum-pad" id="pad-A heater-4">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+              className="clip"
+              id="A"
+            ></audio>
+            "A"
+          </div>
+          <div className="drum-pad" id="pad-S clap">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+              className="clip"
+              id="S"
+            ></audio>
+            "S"
+          </div>
+          <div className="drum-pad" id="pad-D open-HH">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+              className="clip"
+              id="D"
+            ></audio>
+            "D"
+          </div>
+          <div className="drum-pad" id="pad-Z kick-h">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+              className="clip"
+              id="Z"
+            ></audio>
+            "Z"
+          </div>
+          <div className="drum-pad" id="pad-X kick">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+              className="clip"
+              id="X"
+            ></audio>
+            "X"
+          </div>
+          <div className="drum-pad" id="pad-C closed-HH">
+            <audio
+              src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+              className="clip"
+              id="C"
+            ></audio>
+            "C"
+          </div>
+        </div>
+        <div className="display-controls">
+          <div className="controls-power">
+            <p>Power</p>
+            <div className="power-shell">
+              {/* stops all html activations */}
+              <div className="power-shell-slider"></div>
+            </div>
+          </div>
+          <div className="controls-display">
+            {/* displays clickable events */}
+            <p id="display">instrument</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// User Story #1: I should be able to see an outer container with a corresponding id="drum-machine" that contains all other elements.
+
+// User Story #2: Within #drum-machine I can see an element with a corresponding id="display".
+
+// User Story #3: Within #drum-machine I can see 9 clickable drum pad elements, each with a class name of drum-pad, a unique id that describes the audio clip the drum pad will be set up to trigger, and an inner text that corresponds to one of the following keys on the keyboard: Q, W, E, A, S, D, Z, X, C. The drum pads MUST be in this order.
+
+// User Story #4: Within each .drum-pad, there should be an HTML5 audio element which has a src attribute pointing to an audio clip, a class name of clip, and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).
+
+// User Story #5: When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered.
+
+// User Story #6: When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string Q, pressing the W key should trigger the drum pad which contains the string W, etc.).
+
+// User Story #7: When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique).
