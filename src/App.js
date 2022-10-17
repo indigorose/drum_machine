fc704156e0f62436e5a62685ce309a20aa9b9c1c
indigorose <indigorose049@gmail.com>
2022-10-17 21:08:46 +0100

    Add sound and pianoData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,156 @@
 import './App.css';
 import { useState } from 'react';
 function App() {
+  const drumData = [
+    {
+      id: 1,
+      name: 'Heater-1',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      key: 'Q',
+      keyCode: '81',
+    },
+    {
+      id: 2,
+      name: 'Heater-2',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      key: 'W',
+      keyCode: '',
+    },
+    {
+      id: 3,
+      name: 'Heater-3',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      key: 'E',
+      keyCode: '87',
+    },
+    {
+      id: 4,
+      name: 'Heater-4',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      key: 'A',
+      keyCode: '65',
+    },
+    {
+      id: 5,
+      name: 'Clap',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      key: 'S',
+      keyCode: '83',
+    },
+    {
+      id: 6,
+      name: 'Open-HH',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      key: 'D',
+      keyCode: '68',
+    },
+    {
+      id: 7,
+      name: 'Kick-H',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      key: 'Z',
+      keyCode: '90',
+    },
+    {
+      id: 8,
+      name: 'Kick',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      key: 'X',
+      keyCode: '88',
+    },
+    {
+      id: 9,
+      name: 'Closed-HH',
+      audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      key: 'C',
+      keyCode: '67',
+    },
+  ];
+  // const pianoData = [
+  //   {
+  //     id: 1,
+  //     keyCode: 81,
+  //     key: 'Q',
+  //     name: 'Chord-1',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+  //   },
+  //   {
+  //     id: 2,
+  //     keyCode: 87,
+  //     key: 'W',
+  //     name: 'Chord-2',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+  //   },
+  //   {
+  //     id: 3,
+  //     keyCode: 69,
+  //     key: 'E',
+  //     name: 'Chord-3',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+  //   },
+  //   {
+  //     id: 4,
+  //     keyCode: 65,
+  //     key: 'A',
+  //     name: 'Shaker',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+  //   },
+  //   {
+  //     id: 5,
+  //     keyCode: 83,
+  //     key: 'S',
+  //     name: 'Open-HH',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+  //   },
+  //   {
+  //     id: 6,
+  //     keyCode: 68,
+  //     key: 'D',
+  //     name: 'Closed-HH',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  //   },
+  //   {
+  //     id: 7,
+  //     keyCode: 90,
+  //     key: 'Z',
+  //     name: 'Punchy-Kick',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  //   },
+  //   {
+  //     id: 8,
+  //     keyCode: 88,
+  //     key: 'X',
+  //     name: 'Side-Stick',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  //   },
+  //   {
+  //     id: 9,
+  //     keyCode: 67,
+  //     key: 'C',
+  //     name: 'Snare',
+  //     audio: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  //   },
+  // ];
   const [isPower, setIsPower] = useState(false);
   const handleClick = () => {
     setIsPower((current) => !current);
+  };
+  const Keyboard = ({ play }) => {
+    return drumData.map((sound) => {
+      return (
+        <button className="drum-pad" onClick={() => play(sound.key)}>
+          <audio className="clip" id={sound.key} src={sound.audio} />
+          <p>
+            {sound.name} {sound.key}
+          </p>
+        </button>
+      );
+    });
+  };
+  const play = (key) => {
+    const audio = document.getElementById(key);
+    audio.currentTime = 0;
+    audio.play();
   };
   return (
     <div className="App">
@@ -12,14 +159,44 @@ function App() {
       </header>
       <div className="container" id="drum-machine">
         <div className="display-pad">
-          {/* all drum pads are clickable events */}
-          <div className="drum-pad" id="pad-Q heater-1">
+          <Keyboard play={play} />
+        </div>
+        <div className="display-controls">
+          <div className="controls-power">
+            <p id="display">Power</p>
+            <div className="power-shell">
+              {/* stops all html activations */}
+              <div
+                className="power-shell-slider"
+                style={{ float: isPower ? 'left' : 'right' }}
+                onClick={handleClick}
+              ></div>
+            </div>
+          </div>
+          <div className="controls-display">
+            {/* displays clickable events */}
+            <p id="display">Instrument</p>
+            <p>instrument played</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+// {
+/* <div className="display-pad">
+          {/* all drum pads are clickable events */
+// }
+// {
+/* <div className="drum-pad" id="pad-Q heater-1">
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
               className="clip"
               id="Q"
             ></audio>
-            "Q"
+            <p>{drumData[0].name}</p>
           </div>
           <div className="drum-pad" id="pad-W heater-2">
             <audio
@@ -85,32 +262,8 @@ function App() {
             ></audio>
             "C"
           </div>
-        </div>
-        <div className="display-controls">
-          <div className="controls-power">
-            <p id="display">Power</p>
-            <div className="power-shell">
-              {/* stops all html activations */}
-              <div
-                className="power-shell-slider"
-                style={{ float: isPower ? 'left' : 'right' }}
-                onClick={handleClick}
-              ></div>
-            </div>
-          </div>
-          <div className="controls-display">
-            {/* displays clickable events */}
-            <h5>Instrument</h5>
-            <p id="display">instrument played</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
+        </div> */
+// }
 // User Story #1: I should be able to see an outer container with a corresponding id="drum-machine" that contains all other elements.
 
 // User Story #2: Within #drum-machine I can see an element with a corresponding id="display".
